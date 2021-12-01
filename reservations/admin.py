1c354678d9ed54d5c607a951d03a426c226fc9d5
kevinfAAA <kevin.foley21@hotmail.com>
2021-12-01 21:01:15 +0000

    add reservations model to the django admin site

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Reservations
+from django_summernote.admin import SummernoteModelAdmin
+
+
+@admin.register(Reservations)
+class ReservationsAdmin(SummernoteModelAdmin):
+
+    summernote_fields = ('comments')
 
 # Register your models here.
