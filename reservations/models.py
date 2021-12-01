1421dfa8e5f270b04d0b49399197e6a0f19c9d6f
kevinfAAA <kevin.foley21@hotmail.com>
2021-12-01 20:19:20 +0000

    add reservations model

--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
+class Reservations(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user")
+    number_of_quests = models.IntegerField
+    Date = models.DateField(auto_now_add=False)
+    Time = models.TimeField(auto_now_add=False)
+    updated_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    comments = models.TextField()
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.title
+
